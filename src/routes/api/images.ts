13a1a30a1a66db8923bc6bc324e206e81ad2866b
JEFF ENG <ME@JEFFENG.COM>
2022-05-22 16:50:38 -0700

    Added paramchecker middleware to check that user provided filename, height, and width.

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,19 +1,13 @@
 import express from 'express';
+import paramchecker from '../../utilities/paramchecker';
 
 // Create individual route object for images
 const images = express.Router();
 
-images.get('/', (req, res) => {
-    const queryObjectLength = Object.keys(req.query).length;
-    
-    if ( queryObjectLength === 0) {
-        const instructions = 'Endpoint format should be: \/api\/images?filename=example&width=200&height=200';
-        res.send(instructions);
-    } else if (queryObjectLength > 0) {
-        res.sendStatus(200);
-    } 
-
-
+// Images endpoint
+images.get('/', paramchecker, (req, res) => {
+    res.send('Hello');
+    console.log('/images endpoint');
 });
 
 // Export module
