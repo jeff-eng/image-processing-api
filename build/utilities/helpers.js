ee7803f9dbe93e91c562888fa2559030214ecbd4
JEFF ENG <ME@JEFFENG.COM>
2022-06-14 21:24:25 -0700

    Ran ESLint and Prettier scripts.

--- build/utilities/helpers.js
+++ build/utilities/helpers.js
@@ -5,7 +5,8 @@
 // the return data as the second item. The error received from the catch is the first array item.
 Object.defineProperty(exports, "__esModule", { value: true });
 var to = function (promise) {
-    return promise.then(function (data) {
+    return promise
+        .then(function (data) {
         return [null, data];
     })
         .catch(function (err) { return [err]; });
